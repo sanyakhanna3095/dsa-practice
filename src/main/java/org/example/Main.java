170066461753ec0cce61acdf107782b26ea55db9
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-05-06 12:11:58 +0530

    [Sanya] Add: Maximum Hourglass Sum code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,44 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    static final int[][]key={{0, 0},{0,1}, {0,2}, {1,1}, {2,0}, {2,1}, {2,2}};
+//    static final int[] point={0,1,2};
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+//        int[][] arr=new int[6][6];
+//        for(int i=0;i<6;i++){
+//            for(int j=0;j<6;j++){
+//                arr[i][j]=sc.nextInt();
+//            }
+//        }
+
+        int arr[][] =
+                {
+                        {-9, -9, -9,  1, 1, 1},
+                        { 0, -9,  0,  4, 3, 2},
+                        {-9, -9, -9,  1, 2, 3},
+                        { 0,  0,  8,  6, 6, 0},
+                        { 0,  0,  0, -2, 0, 0},
+                        { 0,  0,  1,  2, 4, 0}
+                };
+        System.out.println(solBruteForce(arr));
+    }
+
+    static int solBruteForce(int[][] arr){
+        int res=Integer.MIN_VALUE;
+//        int sum=0;
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                int sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+ arr[i+1][j+1]+ arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+                System.out.print(sum+" ");
+                res=Math.max(res,sum);
+            }
+        }
+        System.out.println();
+        System.out.println("_______");
+
+        return res;
     }
 }
