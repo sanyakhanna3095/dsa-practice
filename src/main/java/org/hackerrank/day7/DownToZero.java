3a4b125c0290e844b99414de2cce63885f3de3fe
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-05-13 15:14:28 +0530

    [Sanya] Add: minimum steps to reduce number to 0 using dp

---
+++ src/main/java/org/hackerrank/day7/DownToZero.java
@@ -0,0 +1,64 @@
+package org.hackerrank.day7;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DownToZero {
+    static int[] memo;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int q=sc.nextInt();
+//        for(int i=0;i<q;i++){
+//            int n=sc.nextInt();
+//            memo = new int[n + 1];
+//            Arrays.fill(memo, -1);
+//            int res=topDown(n);
+//            System.out.println(res);
+//        }
+        int[] query=new int[q];
+        int maxN=0;
+        for(int i=0;i<q;i++){
+            query[i]=sc.nextInt();
+            maxN=Math.max(maxN, query[i]);
+        }
+        int[] res=bottomUp(maxN);
+
+        for (int i = 0; i < q; i++) {
+            System.out.println(res[query[i]]);
+        }
+    }
+    static int topDown(int n){
+        if(n==0) return 0;
+        if(memo[n]!=-1) return memo[n];
+        int res=topDown(n-1)+1;
+        for(int a=2;a*a<=n;a++){
+            if(n%a==0){
+                int b=n/a;
+                int step=Math.max(a,b);
+                res=Math.min(res, topDown(step)+1);
+            }
+        }
+        memo[n]=res;
+        return res;
+    }
+
+    static int[] bottomUp(int n){
+        int[] dp=new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i-1]+1;
+            for (int a = 2; a * a <= i; a++) {
+                if (i % a == 0) {
+                    int b = i / a;
+                    if (a > 1 && b > 1) {
+                        int step=Math.max(a,b);
+                        dp[i] = Math.min(dp[i], dp[step] + 1);
+                    }
+                }
+            }
+        }
+        return dp;
+    }
+
+}
